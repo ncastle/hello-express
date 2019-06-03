370d5dcec584cbbe38d770bae6bea0458210c993
Nickolas.Castle <ncastle@uvm.edu>
2019-06-03 15:30:39 -0400

    query

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get('/', (request, response) => {
-  response.send('Hello, Burlington Code Academy. This is day one stuffs');
+app.get('/hello/:friend', (request, response) => {
+  response.send('Hello, ' + request.params.friend + '!');
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
